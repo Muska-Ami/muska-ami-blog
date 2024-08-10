c4bdf0e82029c1d9a0d55527fca7e69360a7cdce
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-08-10 14:27:50 +0800

    to be continued

--- source/js/followMouseBlur.js
+++
@@ -1,8 +0,0 @@
-function loadFollow() {
-    document.querySelectorAll('follow-mouse-blur').forEach((el) => {
-        el.addEventListener('mousemove'), (e) => {
-            el.style.setProperty('--x', e.offsetX / e.target.offsetWidth)
-            el.style.setProperty('--y', e.offsetY / e.target.offsetHeight)
-        }
-    })
-}
--- source/js/init.js
+++ source/js/init.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", (e) => {
   clickEffect();
   sakuraGenerate();
-  loadFollow();
 
   window.addEventListener("devtoolschange", (ev) => {
     if (ev.detail.open) {
